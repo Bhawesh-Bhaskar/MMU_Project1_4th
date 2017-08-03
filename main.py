1c9cad98a5d1b6c910d85c2d4b9033c2dabc2a86
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2017-08-03 12:04:53 +0530

    spy identity created..

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print "Let's get started!"
+spy_name = raw_input("Provide your name here :")
+spy_slautation = raw_input("What should we all you ? : ")
+# concatination of salutation and name.
+spy_name = spy_slautation + " " + spy_name
+print 'Welcome ' + spy_name + " Glad to have you back with us."

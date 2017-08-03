613d192ef8ea9381fcabe2d18e070758665bf48d
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2017-08-03 12:28:12 +0530

    conditional statement introdused..

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 print "Let's get started!"
 spy_name = raw_input("Provide your name here :")
 spy_slautation = raw_input("What should we all you ? : ")
+# chek wether spy has input something or not
+if len(spy_name) > 0 :
+    #code block if the condition is true.
 # concatination of salutation and name.
 spy_name = spy_slautation + " " + spy_name
 print 'Welcome ' + spy_name + " Glad to have you back with us."

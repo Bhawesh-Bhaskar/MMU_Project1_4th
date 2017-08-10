cfeccdbe49d04aaff0e42167eeff566fe46611c7
Sandeep Kumar <kumarsandeep91@gmail.com>
2017-08-11 00:52:47 +0530

    start_chat() function modifications..

--- start_chat.py
+++ start_chat.py
@@ -1,15 +1,30 @@
 # start_chat() function definition.
 def start_chat(name, age, rating, status):
-    show_menu = True
-    while show_menu:
-        menu_choices = "What do you want to do ? \n 1. Add Status. \n 2. Close Application"
-        result = int(raw_input(menu_choices))
+    # current status message is initialized to None.
+    current_status_message = None
 
-        # validating users input
-        if (result == 1):
-            # action 1
-            pass
-        elif (result == 2):
-            show_menu = False
-        else:
-            print "wrong choice try again."
+    # validating users details.
+    error_message = None # variable for storing error messages.
+
+    if not (age > 12 and age < 50) :
+        # invalid age.
+        error_message = "Invalid age. Provide correct details."
+        print error_message
+    else:
+        welcome_message = "Authentication complete. Welcome\n Name : " + name + "\nAge: " + str(age) + "\nRating: " + str(rating) + "\nProud to have you onboard"
+        print welcome_message
+
+        # displaying menu for user.
+        show_menu = True
+        while show_menu:
+            menu_choices = "What do you want to do ? \n 1. Add Status. \n 2. Close Application"
+            result = int(raw_input(menu_choices))
+
+            # validating users input
+            if (result == 1):
+                # action 1
+                pass
+            elif (result == 2):
+                show_menu = False
+            else:
+                print "wrong choice try again."

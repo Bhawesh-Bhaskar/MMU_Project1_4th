da48905c34dcbefab07f77092b9b8baa6f89d5fe
Sandeep Kumar <kumarsandeep91@gmail.com>
2017-08-11 00:16:04 +0530

    refactoring code..

--- spy_details.py
+++ spy_details.py
@@ -1,4 +1,6 @@
-spy_name = 'Bond'
+# details of default user.
+spy_name = 'bond'
 spy_salutation = 'Mr.'
-spy_age = 21
+spy_age = 24
 spy_rating = 4.7
+spy_is_online = True
--- start_chat.py
+++ start_chat.py
@@ -2,7 +2,7 @@
 def start_chat(name, age, rating):
     show_menu = True
     while show_menu:
-        menu_choices = "What do you want to dp ? \n 1. Add Status. \n 2. Close Application"
+        menu_choices = "What do you want to do ? \n 1. Add Status. \n 2. Close Application"
         result = int(raw_input(menu_choices))
 
         # validating users input

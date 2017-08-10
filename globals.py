8175ce93775943921bfe751e4872c01a1da08526
Sandeep Kumar <kumarsandeep91@gmail.com>
2017-08-11 01:20:15 +0530

    add new status messages implemented..

--- globals.py
+++ globals.py
@@ -2,3 +2,6 @@
 
 # current status message is initialized to None.
 current_status_message = None
+
+# list to store status messages.
+STATUS_MESSAGES = ['My name is Bond, James Bond', 'Shaken, not stirred.', 'Keeping the British end up, Sir']

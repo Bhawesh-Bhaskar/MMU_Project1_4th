0d1b0e21adb72d3c5e7b050b961225fd9ff34049
Sandeep Kumar <kumarsandeep91@gmail.com>
2017-08-16 18:08:07 +0530

    code refactored using dictionaries.

--- add_friend.py
+++ add_friend.py
@@ -1,9 +1,16 @@
 # import statements.
-from globals import friends_name, friends_age, friends_rating, friends_is_online
+from globals import friends
 
 # add new friends.
 def add_friend():
-    new_name = raw_input("Please add your friend's name: ")
+    new_friend = {
+        'name': '',
+        'salutation': '.',
+        'age': 0,
+        'rating': 0.0,
+        'is_online': False
+    }
+    new_friend['name'] = raw_input("Please add your friend's name: ")
     new_salutation = raw_input("Are they Mr. or Ms.?: ")
 
     # concatination.
@@ -15,10 +22,7 @@ def add_friend():
 
     # users input validations
     if len(new_name) > 0 and new_age > 12 and new_age < 50:
-        friends_name.append(new_name)
-        friends_age.append(new_age)
-        friends_rating.append(new_rating)
-        friends_is_online.append(True)
+        friends.append(new_friend)
         print 'Friend Added!'
     else:
         print 'Sorry! Invalid entry. We can\'t add spy with the details you provided'

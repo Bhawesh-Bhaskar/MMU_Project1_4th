2f5503b2ede2430f039209b405241387a09aec2d
Sandeep Sihari <kumarsandeep91@gmail.com>
2017-08-17 11:59:06 +0530

    send secret message completed..

--- start_chat.py
+++ start_chat.py
@@ -2,6 +2,7 @@
 #from globals import current_status_message
 from add_status import add_status
 from add_friend import add_friend
+from send_message import send_message
 
 # start_chat() function definition.
 def start_chat(name, age, rating, status):
@@ -39,6 +40,8 @@ def start_chat(name, age, rating, status):
             elif (result == 2):
                 number_of_friends = add_friend()
                 print 'You have %d friends' % (number_of_friends)
+            elif(result == 3):
+                send_message()
             elif (result == 6):
                 # close application
                 show_menu = False
